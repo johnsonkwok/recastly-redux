1e170841932de7973d8b5e140cce0b9687d7432d
johnsonkwok <johnsonkwok1@gmail.com>
2018-11-29 11:43:22 -0800

    Implement Video List and Video Player containers

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -3,13 +3,19 @@ import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleVideoSearch from '../actions/search.js';
 
+// Write our mapStateToProps function which should take in state at a min and ownProps
+  // Should return a plain object with the data the component needs
+// Write a mapDispatchToProps func which takes in the dispatch function
+  // Should return a plain object where each field becomes a prop for the component
+  // whose values should be the function to dispatch
+  
 const mapStateToProps = state => {
     return {value: state.value};
 };
 
 const mapDispatchToProps = dispatch => {
     return {handleSearchInputChange: (q) => dispatch(handleVideoSearch(q))};
-}
+};
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
@@ -17,12 +23,5 @@ const mapDispatchToProps = dispatch => {
 //state and dispatch mappings.
 var SearchContainer = connect(mapStateToProps, mapDispatchToProps)(Search);
 
-
-
-// Write our mapStateToProps function which should take in state at a min and ownProps
-  // Should return a plain object with the data the component needs
-// Write a mapDispatchToProps func which takes in the dispatch function
-  // Should return a plain object where each field becomes a prop for the component
-  // whose values should be the function to dispatch
 
 export default SearchContainer;
